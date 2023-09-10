745ee680451480e09a11ddd12c31ee92a940d234
Everton Gonçalves <everton.goncalves@ascbrazil.com.br>
2023-09-10 14:10:26 -0300

    alterando a font para Poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter, Borel, Roboto } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 
-// const inter = Inter({ subsets: ['latin'] })
-const roboto = Roboto({ subsets: ['latin'], weight: ['100', '300' ,'400', '700'] })
+const poppins = Poppins({ subsets: ['latin'], weight: ['100', '300' ,'400', '700'] })
 
 export const metadata: Metadata = {
   title: 'Codever Group',
@@ -15,7 +14,7 @@ interface IProps {children: React.ReactNode}
 export default function RootLayout({children}: IProps) {
   return (
     <html lang="pt-br">
-      <body className={roboto.className}>
+      <body className={poppins.className}>
         {children}
       </body>
     </html>
